fd0078cde5c2bfe731ad6aa6ff2e46fd57aa5869
Jon Lee <jonlee@nichols-ve502-0838.apn.wlan.upenn.edu>
2018-02-24 20:07:04 -0500

    fixed multiple tilt

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -35,20 +35,70 @@ public class BulletController : MonoBehaviour
 		Destroy(gameObject, destroyTime);
 	}
 	
+	GameObject FindClosestEnemy()
+	{
+		GameObject tMin = null;
+		float minDist = Mathf.Infinity;
+
+		Vector3 bulletPos = transform.position;
+		
+		foreach(GameObject e in enemies) {
+			float dist = Vector3.Distance(e.transform.position, bulletPos);
+
+			if(dist < minDist) {
+				tMin = e;
+				minDist = dist;
+			}
+		}
+
+		return tMin;
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		foreach(GameObject e in enemies) {
-			EnemyController enemy = e.GetComponent<EnemyController>();
+		// GameObject closestEnemy = null;
+		// float distLeft = 0;
+		// float distRight = 0;
+		// foreach(GameObject e in enemies) {
+		// 	EnemyController enemy = e.GetComponent<EnemyController>();
 			
-			float distEnemyLeft = Vector3.Distance(this.transform.position, enemy.leftEye.transform.position);
-			float distEnemyRight = Vector3.Distance(this.transform.position, enemy.rightEye.transform.position);
+		// 	if(closestEnemy == null && e != null) {
+		// 		closestEnemy = e;
+		// 		distLeft = Vector3.Distance(this.transform.position, enemy.leftEye.transform.position);
+		// 		distRight = Vector3.Distance(this.transform.position, enemy.rightEye.transform.position);
+		// 		continue;
+		// 	}
 
-			if(!enemy.dodging && distEnemyLeft < 0.5 && distEnemyLeft < distEnemyRight) {
+		// 	EnemyController closest = closestEnemy.GetComponent<EnemyController>();
+			
+		// 	float distEnemyLeft = Vector3.Distance(this.transform.position, enemy.leftEye.transform.position);
+		// 	float distEnemyRight = Vector3.Distance(this.transform.position, enemy.rightEye.transform.position);
+		// 	float closestEnemyLeft = Vector3.Distance(this.transform.position, closest.leftEye.transform.position);
+		// 	float closestEnemyRight = Vector3.Distance(this.transform.position, closest.rightEye.transform.position);
+
+		// 	if(distEnemyLeft > closestEnemyLeft || distEnemyRight > closestEnemyRight) {
+		// 		continue;
+		// 	} 
+		// 	else {
+		// 		closestEnemy = e;
+		// 		distLeft = distEnemyLeft;
+		// 		distRight = distEnemyRight;
+		// 	}
+		// }
+
+		GameObject closestEnemy = FindClosestEnemy();
+		if(closestEnemy != null) {
+			EnemyController enemy = closestEnemy.GetComponent<EnemyController>();
+
+			float distLeft = Vector3.Distance(this.transform.position, enemy.leftEye.transform.position);
+			float distRight = Vector3.Distance(this.transform.position, enemy.rightEye.transform.position);
+
+			if(!enemy.dodging && distLeft < 0.5 && distLeft < distRight) {
 				enemy.DodgeBullet(-1);
 				enemy.dodging = true;
 			}
-			else if(!enemy.dodging && distEnemyRight < 0.5 && distEnemyRight < distEnemyLeft) {
+			else if(!enemy.dodging && distRight < 0.5 && distRight < distLeft) {
 				enemy.DodgeBullet(1);
 				enemy.dodging = true;
 			}
