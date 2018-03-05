7a0a2beff847e37c9d1f0ca0b47d50108c43112a
Jon Lee <jonlee@nichols-ve501-0585.apn.wlan.upenn.edu>
2018-03-05 02:33:57 -0500

    login

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -7,27 +7,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 enum LEVELS
 {
-	SCAVENGER_HUNT 	= 0,
-	SPACE_INVADERS 	= 1,
-	MAIN_MENU		= 4,
+	MAIN_MENU		= 0,
+	SCAVENGER_HUNT 	= 1,
+	SPACE_INVADERS 	= 2,
 	HIGH_SCORES 	= 5,
+	REGISTRATION	= 6,
 }
 
 public class MainGameManager : MonoBehaviour 
 {
+	public GameObject welcome;
+
 	// Use this for initialization
 	void Start () 
 	{
+		// Debug
+		// PlayerPrefs.DeleteAll();
 
+		// First time, have player register with email
+		if(PlayerPrefs.GetString("PlayerEmail") == "") {
+			welcome.SetActive(false);
+			LoadRegistration();
+		}
+		else {
+			welcome.SetActive(true);
+			welcome.GetComponent<TMP_Text>().text = "Welcome back " + PlayerPrefs.GetString("PlayerName") + "!";
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	public void LoadRegistration()
 	{
-		
+		SceneManager.LoadScene((int)LEVELS.REGISTRATION);
 	}
 
 	public void LoadScavengerHunt()
@@ -48,5 +62,21 @@ public class MainGameManager : MonoBehaviour
 	public void LoadHighScores()
 	{
 		SceneManager.LoadScene((int)LEVELS.HIGH_SCORES);
+	}
+}
+
+public struct PlayerScore 
+{
+	public string username;
+	public int score;
+	public int phone;
+	public string email;
+
+	public PlayerScore(string _username, int _score, int _phone, string _email)
+	{
+		username = _username;
+		score = _score;
+		phone = _phone;
+		email = _email;
 	}
 }
