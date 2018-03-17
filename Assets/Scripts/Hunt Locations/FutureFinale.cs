0fddf294508a1d5de11c3b03823cba3595e8e955
Jon Lee <jonlee@nichols-ve502-0594.apn.wlan.upenn.edu>
2018-03-17 16:32:11 -0400

    scavengophia done

--- Assets/Scripts/Hunt Locations/FutureFinale.cs
+++ Assets/Scripts/Hunt Locations/FutureFinale.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FutureFinale : MonoBehaviour {
+public class FutureFinale : MonoBehaviour 
+{
+	public GameObject sophia;
+	public GameObject future;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	// Sophia to Future page
+	public void NextPage()
+	{	
+		sophia.SetActive(false);
+		future.SetActive(true);
+	}
+
+	// Future page to Thank You
+	public void ThankYou()
+	{
+		future.transform.Find("Page1").gameObject.SetActive(false);
+		future.transform.Find("Page2").gameObject.SetActive(true);
 	}
 }
