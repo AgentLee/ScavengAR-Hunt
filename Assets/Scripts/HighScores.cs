4fe0b44118d2e8aeedf8d7978b625955fa2933d4
Jon Lee <jonlee@nichols-ve503-0912.apn.wlan.upenn.edu>
2018-03-27 08:36:25 -0400

    cleanup

---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HighScores : MonoBehaviour 
+{
+	const string privateCode = "toXkBrVLoU-NIPomJOiC4QLbv3lvZzuEmvE6ogLTX5vA";
+	const string publicCode = "5a9c91ff012b300a70a814b4";
+	const string webURL = "http://dreamlo.com/lb/";
+	 
+	public PlayerScore[] scores;
+	public GameObject scoreText;
+
+	void Awake()
+	{
+		// AddHighScore("Jon", 100, 123456789, "jon@email");
+		// AddHighScore("Jon", 150, 123456789, "jonlee@email");
+		// AddHighScore("Shlane", 90, 123456789, "shlane@email");
+		// AddHighScore("Paulo", 75, 123456789, "paulo@email");
+		// AddHighScore("Sombra", 999999, 123456789, "sombra@email");
+		// AddHighScore("Sombra", 999998, 123456789, "sombr3333a@email");
+
+		RetrieveScores();
+	}
+
+	void Update()
+	{
+
+	}
+
+	public void AddHighScore(string user, int score, string phone, string email)
+	{
+		Debug.Log("Updating score for " + user + " with " + score + " at " + email);
+		StartCoroutine(UpdateScores(user, score, phone, email));
+	}
+
+	public void RetrieveScores()
+	{
+		StartCoroutine(GetScoresFromServer());
+	}
+
+	IEnumerator UpdateScores(string user, int score, string phone, string email)
+	{
+		float time = 0.0f;
+		// Use the player's email as the "key" because dreamlo overwrites the scores based on that
+		WWW dbLink = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(email) + "/" + score + "/" + phone + "/" + WWW.EscapeURL(user));
+
+		// Wait till finished uploading
+		while(!dbLink.isDone) {
+			if(time >= 3.0f) {
+				Debug.Log("SHow message");
+			}
+			else {
+				time += Time.deltaTime;
+			}
+
+			yield return dbLink;
+		}
+
+		if(string.IsNullOrEmpty(dbLink.error)) {
+			Debug.Log("Scores updated.");
+		}
+		else {
+			Debug.Log("Error: " + dbLink.error);
+		}
+	}
+
+	private string[] ERROR_MESSAGES = {	"Whoops, something went wrong...Try restarting the app!",
+										"Forgot to feed the hamsters keeping score...Try restarting the app!",
+										"My score keeper went on a coffee break...Try restarting the app!"};
+	IEnumerator GetScoresFromServer()
+	{
+		float time = 0.0f;
+		WWW www = new WWW(webURL + publicCode + "/pipe/");
+
+		// Wait till finished uploading
+		while(!www.isDone) {
+			scoreText.GetComponent<TMP_Text>().text = "Loading";
+			
+			// Timeout message
+			if(time >= 3.0f) {
+				scoreText.GetComponent<TMP_Text>().text += "\n";
+				scoreText.GetComponent<TMP_Text>().text += ERROR_MESSAGES[Random.Range(0, 4)];
+			}
+			else {
+				time += Time.deltaTime;
+			}
+
+			yield return www;
+		}
+
+		if(string.IsNullOrEmpty(www.error)) {
+			print(www.text);
+			FormatScores(www.text);
+		}
+		else {
+			Debug.Log("Error: " + www.error);
+			scoreText.GetComponent<TMP_Text>().text = "Couldn't load scores";
+		}
+	}
+
+	void FormatScores(string textScores)
+	{
+		scoreText.GetComponent<TMP_Text>().text = "";
+		
+		string[] entries = textScores.Split(new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+		scores = new PlayerScore[entries.Length];
+
+		for(int i = 0; i < entries.Length; ++i) {
+			string[] entryInfo 	= entries[i].Split(new char[]{'|'});
+			string email 		= entryInfo[0];
+			int score 			= int.Parse(entryInfo[1]);
+			string phone 		= entryInfo[2];
+			string username 	= entryInfo[3];
+
+			// New user, don't show score
+			if(score == -999999) {
+				continue;
+			}
+
+			scores[i] = new PlayerScore(username, score, phone, email);
+
+			if(i < 5) {
+				if(i == 0) {
+					PlayerPrefs.SetInt("TopScore", scores[i].score);
+				}
+
+				// scoreText.GetComponent<TMP_Text>().text += (i + 1).ToString() + ". " + username + " - " + score + "\n";
+				scoreText.GetComponent<TMP_Text>().text += username + " - " + score + "\n";
+			}
+
+			// print(scores[i].username + "-" + scores[i].email + ": " + scores[i].score + "-" + scores[i].phone);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Registration : MonoBehaviour 
+{
+	public TMP_InputField nameField;
+	public TMP_InputField phoneField;
+	public TMP_InputField emailField;
+
+	private string username;
+	private string phone;
+	private string email;
+
+	public GameObject errorMessage;
+
+	public PlayerScore[] scores;
+	const string privateCode 	= "toXkBrVLoU-NIPomJOiC4QLbv3lvZzuEmvE6ogLTX5vA";
+	const string publicCode 	= "5a9c91ff012b300a70a814b4";
+	const string webURL 		= "http://dreamlo.com/lb/";
+
+	void Start()
+	{
+		errorMessage.SetActive(false);
+
+		RetrieveScores();
+	}
+
+	public void Register()
+	{
+		// emailField gets checked by TMP if it's a valid address
+		if(nameField.text == "" || phoneField.text == "" || emailField.text == "") {
+			Debug.Log("You must fill out the form");
+			
+			errorMessage.SetActive(true);
+		}
+		else {
+			username 	= nameField.text;
+			phone 		= phoneField.text;
+			email 		= emailField.text;
+			
+			Submit();
+		}
+	}
+
+	public void Submit()
+	{
+		// Check to see if user is already registered
+		if(!PlayerRegistered()) {
+			StartCoroutine(RegisterUser());
+		}
+		else {
+			SceneManager.LoadScene((int)LEVELS.MAIN_MENU);
+		}
+	}
+
+	bool PlayerRegistered()
+	{
+		for(int i = 0; i < scores.Length; ++i) {
+			if(scores[i].email == email) {
+				// If the player is already registered,
+				// load in their stats and continue the game.
+
+				PlayerPrefs.SetString("PlayerName", scores[i].username);
+				PlayerPrefs.SetString("PlayerEmail", scores[i].email);
+				PlayerPrefs.SetString("PlayerPhone", scores[i].phone.ToString());
+				PlayerPrefs.SetInt("PlayerScore", scores[i].score);
+				// PlayerPrefs.SetInt("Tilt", 0);
+				// PlayerPrefs.SetInt("Gamepad", 1);
+
+				// This is an issue. I have no way of keeping track of their stats.
+				// PlayerPrefs.SetInt("L2 Shield", 0);			// ENIAC - Shield
+				// PlayerPrefs.SetInt("L3 Shield", 0);			// AddLab - Bar
+				// PlayerPrefs.SetInt("ExtraLife", 0);			// GRASP - Franklin Fish
+				// PlayerPrefs.SetInt("Book", 0);				// SigLab - Book
+				// PlayerPrefs.SetInt("BulletSpread", 0);		// SigLab - Penn Dots
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public void RetrieveScores()
+	{
+		StartCoroutine(GetScoresFromServer());
+	}
+
+	IEnumerator RegisterUser()
+	{
+		// Use the player's email as the "key" because dreamlo overwrites the scores based on that
+		WWW dbLink = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(email) + "/" + -999999 + "/" + phone + "/" + WWW.EscapeURL(username));
+
+		// Wait till finished uploading
+		yield return dbLink;
+
+		if(string.IsNullOrEmpty(dbLink.error)) {
+			Debug.Log("Player registered.");
+
+			// PlayerPrefs.SetInt("Registered", 1);
+			PlayerPrefs.SetString("PlayerName", username);
+			PlayerPrefs.SetString("PlayerEmail", email);
+			PlayerPrefs.SetString("PlayerPhone", phone);
+			PlayerPrefs.SetInt("PlayerScore", -999999);
+
+			PlayerPrefs.SetInt("L3", 0);				// L3
+			PlayerPrefs.SetInt("WeaponSpread", 0);		// Weapon Spread
+			PlayerPrefs.SetInt("PowerUpShield", 0);		// ENIAC 	- Shield		- L2
+			PlayerPrefs.SetInt("PowerUpBar", 0);		// AddLab 	- Bar			- L3 (if completed)
+			PlayerPrefs.SetInt("PowerUpFish", 0);		// GRASP 	- Franklin Fish	- Life
+			PlayerPrefs.SetInt("PowerUpBook", 0);		// SigLab 	- Book			- 
+			PlayerPrefs.SetInt("PowerUpPenn", 0);		// SigLab 	- Penn Dots		- 
+			
+			PlayerPrefs.SetInt("Gamepad", 1);		
+			
+			// Power ups
+			// PlayerPrefs.SetInt("PlayerItems", 0);
+
+			// Start game
+			SceneManager.LoadScene((int)LEVELS.MAIN_MENU);
+		}
+		else {
+			Debug.Log("Error: " + dbLink.error);
+		}
+	}
+
+	IEnumerator GetScoresFromServer()
+	{
+		WWW www = new WWW(webURL + publicCode + "/pipe/");
+		
+		// Wait till finished uploading
+		yield return www;
+
+		if(string.IsNullOrEmpty(www.error)) {
+			// print(www.text);
+			FormatScores(www.text);
+		}
+		else {
+			Debug.Log("Error: " + www.error);
+		}
+	}
+
+	void FormatScores(string textScores)
+	{
+		string[] entries = textScores.Split(new char[] {'\n'}, System.StringSplitOptions.RemoveEmptyEntries);
+		scores = new PlayerScore[entries.Length];
+
+		for(int i = 0; i < entries.Length; ++i) {
+			string[] playerInfo = entries[i].Split(new char[]{'|'});
+			
+			string email 	= playerInfo[0];
+			int score 		= int.Parse(playerInfo[1]);
+			string phone 	= playerInfo[2];
+			string username = playerInfo[3];
+
+			// New user
+			if(score == -999999) {
+				continue;
+			}
+
+			scores[i] = new PlayerScore(username, score, phone, email);
+		}
+	}
+
+	public void CloseErrorMessage()
+	{
+		errorMessage.SetActive(false);
+	}
+
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene((int)LEVELS.MAIN_MENU);
+	}
+}
